e413c383ef6d61a79533ad60cd02576c2b7f0b96
Matthew Balnis <matthew.balnis@uconn.edu>
2021-12-29 20:54:08 -0500

    Graphql code generation

---
+++ src/graphql/subscriptions.ts
@@ -0,0 +1,103 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateSession = /* GraphQL */ `
+  subscription OnCreateSession {
+    onCreateSession {
+      id
+      name
+      searchCity
+      searchBounds {
+        bottomLeft {
+          lat
+          lng
+        }
+        topRight {
+          lat
+          lng
+        }
+      }
+      listings {
+        id
+        name
+        address
+        location {
+          lat
+          lng
+        }
+        price
+        numberOfBedrooms
+        numberOfBathrooms
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateSession = /* GraphQL */ `
+  subscription OnUpdateSession {
+    onUpdateSession {
+      id
+      name
+      searchCity
+      searchBounds {
+        bottomLeft {
+          lat
+          lng
+        }
+        topRight {
+          lat
+          lng
+        }
+      }
+      listings {
+        id
+        name
+        address
+        location {
+          lat
+          lng
+        }
+        price
+        numberOfBedrooms
+        numberOfBathrooms
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteSession = /* GraphQL */ `
+  subscription OnDeleteSession {
+    onDeleteSession {
+      id
+      name
+      searchCity
+      searchBounds {
+        bottomLeft {
+          lat
+          lng
+        }
+        topRight {
+          lat
+          lng
+        }
+      }
+      listings {
+        id
+        name
+        address
+        location {
+          lat
+          lng
+        }
+        price
+        numberOfBedrooms
+        numberOfBathrooms
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
