c518bdd205f3694e587acc8cfb1dd7c18cd9042e
Matthew Balnis <matthew.balnis@uconn.edu>
2021-12-30 16:39:09 -0500

    Adding auth resource

--- src/index.tsx
+++ src/index.tsx
@@ -5,32 +5,24 @@ import { ReactQueryDevtools } from "react-query/devtools";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import awsmobile from "./aws-exports";
 
-import Amplify from "aws-amplify";
-// import Initialize from "./Initialize";
+import Initialize from "./Initialize";
 import AuthorizationContextProvider from "./Contexts/AuthorizationContext";
 
-// TODO: add back after auth is created
-// Initialize();
-
-awsmobile.aws_appsync_authenticationType = "API_KEY";
-console.log("configuration:");
-console.log(awsmobile);
-Amplify.configure(awsmobile);
+Initialize();
 
 const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <AuthorizationContextProvider> */}
-    <QueryClientProvider client={queryClient}>
-      <div className="mainContainer">
-        <App />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </div>
-    </QueryClientProvider>
-    {/* </AuthorizationContextProvider> */}
+    <AuthorizationContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <div className="mainContainer">
+          <App />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </div>
+      </QueryClientProvider>
+    </AuthorizationContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
