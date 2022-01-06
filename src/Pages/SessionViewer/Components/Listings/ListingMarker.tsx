2e40912aeff98f8ad24c6fa166e47aa018154751
Matthew Balnis <matthew.balnis@uconn.edu>
2022-01-05 22:06:09 -0500

    Trying to create a context menu for markers

--- src/Pages/SessionViewer/Components/Listings/ListingMarker.tsx
+++ src/Pages/SessionViewer/Components/Listings/ListingMarker.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import classNames from "classnames";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Popover from "react-bootstrap/Popover";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { Listing } from "../../../../Models/Session";
@@ -9,12 +11,22 @@ export default function ListingMarker({
   lat,
   lng,
   hovered,
+  listing,
 }: ListingMarkerProps) {
+  const [showContextMenu, setShowContextMenu] = React.useState(false);
+
   return (
-    <FontAwesomeIcon
-      className={classNames(styles.container, hovered && styles.hovered)}
-      icon={faHome}
-    />
+    <OverlayTrigger
+      show={showContextMenu}
+      placement="right"
+      overlay={<ListingMarkerContextMenu listing={listing} />}
+    >
+      <FontAwesomeIcon
+        className={classNames(styles.container, hovered && styles.hovered)}
+        icon={faHome}
+        onContextMenu={() => setShowContextMenu((prev) => !prev)}
+      />
+    </OverlayTrigger>
   );
 }
 
@@ -23,4 +35,16 @@ export interface ListingMarkerProps {
   hovered?: boolean;
   lat: number;
   lng: number;
+}
+
+function ListingMarkerContextMenu({ listing }: { listing: Listing }) {
+  return (
+    <Popover id="popover-basic">
+      <Popover.Title as="h3">{listing.name}</Popover.Title>
+      <Popover.Content>
+        And here's some <strong>amazing</strong> content. It's very engaging.
+        right?
+      </Popover.Content>
+    </Popover>
+  );
 }
