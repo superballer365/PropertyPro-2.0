0f63f670b3aa036913828d2afc1396f8fd93de3c
superballer365 <matthew.balnis@uconn.edu>
2022-01-16 16:13:49 -0500

    Adding a very basic roommates page with a query for all of the users in the user pool

--- src/Pages/SessionViewer/Components/Roommates/RoommatesPanel.tsx
+++ src/Pages/SessionViewer/Components/Roommates/RoommatesPanel.tsx
@@ -1,5 +1,29 @@
 import React from "react";
+import AWS from "aws-sdk";
+import Auth from "@aws-amplify/auth";
 
 export default function RoommatesPanel() {
+  const dod = async () => {
+    const cred = await Auth.currentCredentials();
+    const c = new AWS.CognitoIdentityServiceProvider({
+      region: "us-east-1",
+      credentials: Auth.essentialCredentials(cred),
+    });
+    c.listUsers(
+      {
+        UserPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID!,
+        AttributesToGet: ["email"],
+      },
+      (err, data) => {
+        console.log("Error:" + err);
+        console.log(data.Users);
+      }
+    );
+  };
+
+  React.useEffect(() => {
+    dod();
+  }, []);
+
   return <div>Roommates</div>;
 }
