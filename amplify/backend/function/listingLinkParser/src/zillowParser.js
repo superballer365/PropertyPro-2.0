1873dee1b0c7ea8004989f88d8b3acaa1af2d074
superballer365 <matthew.balnis@uconn.edu>
2022-01-29 17:43:13 -0500

    Adding zillow parsing for bed/bath/price

--- amplify/backend/function/listingLinkParser/src/zillowParser.js
+++ amplify/backend/function/listingLinkParser/src/zillowParser.js
@@ -1,4 +1,5 @@
 const { parse } = require("node-html-parser");
+const { isNumeric } = require("./utils");
 
 async function parseZillowPage(page) {
   const pageContent = await page.content();
@@ -9,6 +10,65 @@ async function parseZillowPage(page) {
 function parseZillowHtmlString(htmlString) {
   const root = parse(htmlString);
 
+  const images = parseImages(root);
+  const beds = parseBeds(root);
+  const baths = parseBaths(root);
+  const price = parsePrice(root);
+
+  return {
+    price,
+    numberOfBedrooms: beds,
+    numberOfBathrooms: baths,
+    pictures: images,
+  };
+}
+
+function parsePrice(root) {
+  try {
+    const summaryRowElement = root.querySelector(".ds-summary-row");
+    const priceElement = summaryRowElement.firstChild.firstChild.firstChild;
+    let priceString = priceElement.text;
+    // remove first character, which should be '$'
+    priceString = priceString.replace(",", "").substring(1);
+    return isNumeric(priceString) ? parseFloat(priceString) : undefined;
+  } catch (e) {
+    console.log("Failed to parse price");
+    console.error(e);
+    return undefined;
+  }
+}
+
+function parseBeds(root) {
+  try {
+    const livingAreaContainer = root.querySelector(
+      ".ds-bed-bath-living-area-container"
+    );
+    const bedsElement = livingAreaContainer.firstChild.firstChild;
+    let bedsString = bedsElement.text;
+    return isNumeric(bedsString) ? parseFloat(bedsString) : undefined;
+  } catch (e) {
+    console.log("Failed to parse beds");
+    console.error(e);
+    return undefined;
+  }
+}
+
+function parseBaths(root) {
+  try {
+    const livingAreaContainer = root.querySelector(
+      ".ds-bed-bath-living-area-container"
+    );
+    const bathsElement = livingAreaContainer.childNodes[2].firstChild;
+    let bathsString = bathsElement.text;
+    return isNumeric(bathsString) ? parseFloat(bathsString) : undefined;
+  } catch (e) {
+    console.log("Failed to parse baths");
+    console.error(e);
+    return undefined;
+  }
+}
+
+function parseImages(root) {
   let imageUrls = [];
   // TODO: fix the fact that this only gets the first 7 images
   const mediaStreamTiles = root.querySelectorAll(".media-stream-tile");
