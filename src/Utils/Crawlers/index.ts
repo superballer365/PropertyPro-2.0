65a3fee1995284e8350138c7ec76b58608b5e704
superballer365 <matthew.balnis@uconn.edu>
2022-02-06 18:07:12 -0500

    Properly handling graphQL errors

--- src/Utils/Crawlers/index.ts
+++ src/Utils/Crawlers/index.ts
@@ -9,6 +9,9 @@ export async function crawlLink(
   const response = (await API.graphql(
     graphqlOperation(getListingInfo, { listingUrl })
   )) as any;
+
+  if (response.errors && response.errors.length > 0) throw response.errors[0];
+
   const data = response.data?.getListingInfo as GetListingInfoResponse;
   return data;
 }
--- src/graphql/callGraphQL.ts
+++ src/graphql/callGraphQL.ts
@@ -14,10 +14,27 @@ async function callGraphQL<T>(
   query: any,
   options?: GraphQLOptions
 ): Promise<GraphQLResult<T>> {
-  return (await API.graphql({
-    ...graphqlOperation(query, options),
-    authMode: "AMAZON_COGNITO_USER_POOLS",
-  })) as GraphQLResult<T>;
+  try {
+    const resp = (await API.graphql({
+      ...graphqlOperation(query, options),
+      authMode: "AMAZON_COGNITO_USER_POOLS",
+    })) as GraphQLResult<T>;
+
+    // check for errors on response
+    if (resp.errors && resp.errors.length > 0) throw resp.errors[0];
+
+    return resp;
+  } catch (e) {
+    if (e instanceof Error) throw e;
+
+    // for some reason, API.graphql will throw the whole response object as an error
+    // if something goes wrong, so try to pull the first error from the response and rethrow it
+    if (e.errors && Array.isArray(e.errors) && e.errors.length > 0)
+      throw e.errors[0];
+
+    // We don't know what this is, so just throw a generic error
+    throw new Error("Failed to make graphQL request");
+  }
 }
 
 export default callGraphQL;
