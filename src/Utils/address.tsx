9c56c49b8db161b885a5640c37358eda558a4112
superballer365 <matthew.balnis@uconn.edu>
2022-02-12 16:16:55 -0500

    Minor display improvement for listing viewer

--- src/Utils/address.tsx
+++ src/Utils/address.tsx
@@ -1,4 +1,4 @@
 export function getAddressComponents(addressString: string) {
   const components = addressString.split(", ");
-  return { street: components[0], state: components[1] };
+  return { street: components[0], city: components[1], state: components[2] };
 }
