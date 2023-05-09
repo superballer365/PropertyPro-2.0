ba6ec601fc9b5dbd08eea5e761750f60965035aa
superballer365 <matthew.balnis@uconn.edu>
2023-05-08 22:40:23 -0400

    Adding street view preview

--- src/Utils/address.tsx
+++ src/Utils/address.tsx
@@ -2,3 +2,10 @@ export function getAddressComponents(addressString: string) {
   const components = addressString.split(", ");
   return { street: components[0], city: components[1], state: components[2] };
 }
+
+export function getStreetViewUrlFromAddress(
+  address: string,
+  size: { height: number; width: number } = { height: 400, width: 300 }
+) {
+  return `https://maps.googleapis.com/maps/api/streetview?size=${size.height}x${size.width}&location=${address}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
+}
